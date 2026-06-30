e9487f5d3fe0d86dc7f077f1808bba286beb942e
Kay <info@beckmann-md.de>
2026-06-30 11:40:25 +0200

    build(web): Flutter Web-App neu gebaut (RBAC-Verfeinerung, Sync-Server-Vorrang)

--- docker/app_web/flutter_bootstrap.js
+++ docker/app_web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1232417118" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3975573985" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
